79e71204b8590111416c3cadf21f5b9a733fae6b
Alan Edwardes <alan@alanedwardes.com>
2018-04-12 23:05:41 +0100

    Adds BitExtensions, more work on VS_FIXEDFILEINFO.

---
+++ src/BinaryMapper.Core/BitExtensions.cs
@@ -0,0 +1,21 @@
+﻿using DWORD = System.UInt32;
+using WORD = System.UInt16;
+
+namespace BinaryMapper.Core
+{
+    public static class BitExtensions
+    {
+        /// <summary>
+        /// See LOWORD in WINDEF.H
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static WORD LowWord(this DWORD value) => (WORD)value;
+        /// <summary>
+        /// See HIWORD in WINDEF.H
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static WORD HighWord(this DWORD value) => (WORD)((value >> 16) & 0xFFFF);
+    }
+}
--- src/BinaryMapper.Minidump/Structures/VS_FIXEDFILEINFO.cs
+++ src/BinaryMapper.Minidump/Structures/VS_FIXEDFILEINFO.cs
@@ -1,4 +1,6 @@
-﻿using DWORD = System.UInt32;
+﻿using BinaryMapper.Core;
+using System;
+using DWORD = System.UInt32;
 
 namespace BinaryMapper.Structures
 {
@@ -13,14 +15,53 @@ namespace BinaryMapper.Structures
         public DWORD dwFileVersionLS;
         public DWORD dwProductVersionMS;
         public DWORD dwProductVersionLS;
-        public DWORD dwFileFlagsMask;
-        public DWORD dwFileFlags;
-        public DWORD dwFileOS;
-        public DWORD dwFileType;
+        public dwFileFlags dwFileFlagsMask;
+        public dwFileFlags dwFileFlags;
+        public dwFileOS dwFileOS;
+        public dwFileType dwFileType;
         public DWORD dwFileSubtype;
         public DWORD dwFileDateMS;
         public DWORD dwFileDateLS;
 
         public bool IsSignatureValid => dwSignature == 0xFEEF04BD;
+        public Version StructureVersion => new Version(dwStrucVersion.HighWord(), dwStrucVersion.LowWord());
+        public Version FileVersion => new Version(dwFileVersionMS.HighWord(), dwFileVersionMS.LowWord(), dwFileVersionLS.HighWord(), dwFileVersionLS.LowWord());
+        public Version ProductVersion => new Version(dwProductVersionMS.HighWord(), dwProductVersionMS.LowWord(), dwProductVersionLS.HighWord(), dwProductVersionLS.LowWord());
+    }
+
+    [Flags]
+    public enum dwFileFlags : DWORD
+    {
+        VS_FF_DEBUG = 0x00000001,
+        VS_FF_INFOINFERRED = 0x00000010,
+        VS_FF_PATCHED = 0x00000004,
+        VS_FF_PRERELEASE = 0x00000002,
+        VS_FF_PRIVATEBUILD = 0x00000008,
+        VS_FF_SPECIALBUILD = 0x00000020
+    }
+
+    [Flags]
+    public enum dwFileOS : DWORD
+    {
+        VOS_DOS = 0x00010000,
+        VOS_NT = 0x00040000,
+        VOS__WINDOWS16 = 0x00000001,
+        VOS__WINDOWS32 = 0x00000004,
+        VOS_OS216 = 0x00020000,
+        VOS_OS232 = 0x00030000,
+        VOS__PM16 = 0x00000002,
+        VOS__PM32 = 0x00000003,
+        VOS_UNKNOWN = 0x00000000
+    }
+
+    public enum dwFileType : DWORD
+    {
+        VFT_APP = 0x00000001,
+        VFT_DLL = 0x00000002,
+        VFT_DRV = 0x00000003,
+        VFT_FONT = 0x00000004,
+        VFT_STATIC_LIB = 0x00000007,
+        VFT_UNKNOWN = 0x00000000,
+        VFT_VXD = 0x00000005
     }
 }
