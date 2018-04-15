36f007fe3c4347ded26bb6fb222a72746afe13e6
Alan Edwardes <alan@alanedwardes.com>
2018-04-15 12:31:46 +0100

    Adds ICONDIR and ICONDIRENTRY.

---
+++ src/BinaryMapper.Windows.Executable/Structures/ICONDIR.cs
@@ -0,0 +1,14 @@
+﻿using BinaryMapper.Core.Attributes;
+using WORD = System.UInt16;
+
+namespace BinaryMapper.Windows.Executable.Structures
+{
+    public class ICONDIR
+    {
+        public WORD idReserved;
+        public WORD idType;
+        public WORD idCount;
+        [ArraySize(nameof(idCount))]
+        public ICONDIRENTRY[] idEntries;
+    }
+}
---
+++ src/BinaryMapper.Windows.Executable/Structures/ICONDIRENTRY.cs
@@ -0,0 +1,22 @@
+﻿
+using BYTE = System.Byte;
+using WORD = System.UInt16;
+using DWORD = System.UInt32;
+using BinaryMapper.Core;
+
+namespace BinaryMapper.Windows.Executable.Structures
+{
+    public class ICONDIRENTRY
+    {
+        public BYTE bWidth;
+        public BYTE bHeight;
+        public BYTE bColorCount;
+        public BYTE bReserved;
+        public WORD wPlanes;
+        public WORD wBitCount;
+        public DWORD dwBytesInRes;
+        public DWORD dwImageOffset;
+
+        public SizeSpan dwBytesInResMarshaled => SizeSpan.FromBytes(dwBytesInRes);
+    }
+}
